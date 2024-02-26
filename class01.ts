5a123d4dd84c0ee5caf82949a7e052565f8fcc8a
Muhammad Bilal Hussain <bilalhussain42201@gmail.com>
2024-02-27 01:25:51 +0500

    commit answer

--- class01.ts
+++ class01.ts
@@ -26,10 +26,10 @@
 
 
 // Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-let person_Name:string =`\t\n   Jogn Doe   \t\n`
-console.log(`Origional: ${person_Name}`);
-console.log(`Stripped: ${person_Name.trim()}`);
 
+// let person_Name:string =`\t\n   Jogn Doe   \t\n`
+// console.log(`Origional: ${person_Name}`);
+// console.log(`Stripped: ${person_Name.trim()}`);
 
 // Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 // You should create four lines that look like this:
@@ -37,22 +37,22 @@ console.log(`Stripped: ${person_Name.trim()}`);
 // Your output should simply be four lines with the number 8 appearing once on each line.
 
 // // Addition operation resulting in 8
-console.log(5 + 3);
+// console.log(5 + 3);
 
 // Subtraction operation resulting in 8
-console.log(10 - 2);
+// console.log(10 - 2);
 
 // Multiplication operation resulting in 8
-console.log(4 * 2);
+// console.log(4 * 2);
 
 // Division operation resulting in 8
-console.log(64 / 8);
+// console.log(64 / 8);
 
 
 
 // Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-let favorite_number:number = 4;
-console.log(`My favorite number is ${favorite_number}`);
+// let favorite_number:number = 4;
+// console.log(`My favorite number is ${favorite_number}`);
 
 // Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 

64ea0bfc9d86e56a316102d0ef67c0a6e6e5fb95
Bilal Hussain <bilalhussain42201@gmail.com>
2024-03-03 01:51:40 +0500

    update changing

--- Assignemt 2 to 10/class01.ts
+++ Assignemt 2 to 10/class01.ts
@@ -20,8 +20,8 @@ console.log(`${Author_Name} once said, ${quote}`);
 //! Q5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
 let famous_person: string = "Albert Einstein";
-let message: string = `${famous_person} once said, ${quote}`
-console.log(message);
+let messages: string = `${famous_person} once said, ${quote}`
+console.log(messages);
 
 //! Q6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
 
